dd6e4e33f593f555e1145b12943168ccb2c31fee
caosva01 <caosva@hotmail.com>
2025-04-09 20:19:17 -0500

    estructura inical de algoritomos para calcular una nota

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Desarrollar un sistema de notas para un estudiante que desea saber su nota final( de cada materia), el sistema debe pedir el nombre del estudiante,
+        // el documento y el programa al que esta matriculado (Menu de opciones: Desarrollo de Software, diseño grafico, gastronomia)
+        // Debe permitir seleccionar la materia (Menu de opciones:
+        // Desarrollo: logica, metodologias, introduccion
+        // Diseño: tipografia, iconografia, semantica
+        // Gastronomia: costos, panaderia dulce, cocina internacional
+        // El calculo de notas es de la siguiente manera:
+        // Para cada materia hay un examen parcial que equivale al 20% de la nota, un taller que equivale el 30% de la nota y un examen final equivale
+        // el 50% de la nota
+        // Una vez calculada la nota final tener en cuenta que:
+        // Si la nota esta entre 0 y 2 el estudiante pierde definitivamente
+        // Si la nota esta entre 2.1 y 2.9  tiene derecho a pla de mejora
+        // Si la nota es igual o superior de 3 , asi que que le queda la materia
+        // Si el estudiante hace un plan de mejoramiento, la nota maxima de la materia sera 3
+        //
+
+
+
+
+
+
+    }
+}
